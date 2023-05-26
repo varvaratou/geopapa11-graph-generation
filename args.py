c06ed9bd52e17ce3bdc4276b2ee4fc5062a797a6
geopapa11 <gio.fou@gmail.com>
2023-05-26 11:18:52 -0400

    - Removing argument `cuda`, since I am introducing argument `device` that detects whether there's CUDA running in the background automatically. - Introducing arguments `train_size_frac`, `valid_size_frac` where you can set the training and validation dataset sizes, respectively. - Introducing argument `vocab_size_node_label` that allows you to set nodes of different types (a type is represented as a label). - Introducing argument `embedding_size_node_label` that lets you define the embedding of a node label.

--- args.py
+++ args.py
@@ -1,4 +1,6 @@
 import os
+import torch
+
 
 # program configuration
 class Args:
@@ -6,7 +8,8 @@ class Args:
         # if clean tensorboard
         self.clean_tensorboard = True
         # Which CUDA GPU device is used for training
-        self.cuda = 1
+        # self.cuda = 0
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
         # Which GraphRNN model variant is used.
         self.note = 'GraphRNN_MLP'  # the simple version of Graph RNN
@@ -25,7 +28,6 @@ class Args:
         # self.graph_type = 'grid'
         self.graph_type = 'grid_small'
         # self.graph_type = 'ladder_small'
-
         # self.graph_type = 'enzymes'
         # self.graph_type = 'enzymes_small'
         # self.graph_type = 'barabasi'
@@ -39,32 +41,40 @@ class Args:
         # if self.graph_type == 'barabasi_noise':
         #     self.graph_type = self.graph_type + str(self.noise)
 
-        # if none, then auto calculate
+        self.train_size_frac = 0.8  # what fraction of the original data will correspond to training data
+        self.valid_size_frac = 0.2  # what fraction of the original data will correspond to validation data
+
+        # if None, then auto calculate
         self.max_num_node = None  # max number of nodes in a graph
         self.max_prev_node = None  # max number of previous nodes that we look back
+
+        # This argument controls whether there are nodes of different types or just one (in that case it is set to None)
+        # Set it to something other than None, if there is more than one type of nodes
+        self.vocab_size_node_label = None  # 10  # None, 10
 
         # network config
         # GraphRNN
         self.parameter_shrink = 2 if 'small' in self.graph_type else 1
-        self.hidden_size_rnn = int(128/self.parameter_shrink)  # hidden size for main RNN
+        self.hidden_size_rnn = int((128 if self.vocab_size_node_label is None else 384) / self.parameter_shrink)  # main RNN hidden size
         self.hidden_size_rnn_output = 16  # hidden size for output RNN
-        self.embedding_size_rnn = int(64/self.parameter_shrink)  # the size for LSTM input
-        self.embedding_size_rnn_output = 8  # the embedding size for output rnn
+        self.embedding_size_rnn = int((64 if self.vocab_size_node_label is None else 128) / self.parameter_shrink)  # RNN input size
+        self.embedding_size_node_label = int(64 / self.parameter_shrink)  # the embedding size for a node's label
+        self.embedding_size_rnn_output = 8  # the embedding size for output RNN
         self.embedding_size_output = int(64/self.parameter_shrink)  # the embedding size for output (VAE/MLP)
 
-        self.batch_size = 32  # normal: 32, and the rest should be changed accordingly
-        self.test_batch_size = 32
-        self.test_total_size = 1000
-        self.num_layers = 4
+        self.batch_size = 32  # 32, 8  # normal: 32, and the rest should be changed accordingly (JiaxuanYou set this to 32)
+        self.test_batch_size = 32  # how many test examples to generate in a batch
+        self.test_total_size = 1000  # how many test examples to generate in total
+        self.num_layers = 4  # number of RNN layers
 
         # training config
         self.num_workers = 4  # num workers to load data, default 4
-        self.batch_ratio = 32  # how many batches of samples per epoch, default 32, e.g., 1 epoch = 32 batches
-        self.epochs = 3000  # now one epoch means self.batch_ratio x batch_size
-        self.epochs_test_start = 100
-        self.epochs_test = 100
-        self.epochs_log = 100
-        self.epochs_save = 10
+        self.batch_ratio = 32  # how many batches are in an epoch, default 32, e.g., 1 epoch = 32 batches
+        self.epochs = 100  # 100, 3000 one epoch means self.batch_ratio x batch_size samples (JiaxuanYou set this to 3000)
+        self.epochs_test_start = 10  # 10, 100 (JiaxuanYou set this to 100) start testing after `epochs_test_start` epochs
+        self.epochs_test = 10  # 10, 100 (JiaxuanYou set this to 100) test every `epochs_test` epochs
+        self.epochs_log = 10  # 10, 100 (JiaxuanYou set this to 100) log every `epochs_log` epochs
+        self.epochs_save = 10  # save model every `epochs_save` epochs
 
         self.lr = 0.003
         self.milestones = [400, 1000]
@@ -73,7 +83,7 @@ class Args:
         self.sample_time = 2  # sample time in each time step, when validating
 
         # output config
-        # self.dir_input = '/dfs/scratch0/jiaxuany0/
+        # self.dir_input = '/dfs/scratch0/
         self.dir_input = './'
         self.model_save_path = os.path.join(self.dir_input, 'model_save')  # only for nll evaluation
         self.graph_save_path = os.path.join(self.dir_input, 'graphs')
@@ -83,7 +93,7 @@ class Args:
         self.nll_save_path = os.path.join(self.dir_input, 'nll')
 
         self.load = False  # if load model, default lr is very low
-        self.load_epoch = 3000
+        self.load_epoch = min(3000, self.epochs)
         self.save = True
 
         # baseline config
@@ -96,6 +106,8 @@ class Args:
 
         # filenames to save intermediate and final outputs
         self.fname = f'{self.note}_{self.graph_type}_{self.num_layers}_{self.hidden_size_rnn}_'
+        self.real_graph_filename = None
+        self.fname_real = f'{self.fname}real_'
         self.fname_pred = f'{self.fname}pred_'
         self.fname_train = f'{self.fname}train_'
         self.fname_test = f'{self.fname}test_'
